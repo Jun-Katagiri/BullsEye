faef36772baec9605caf6d422a787a1e3b43191c
Jun Katagiri <jun@katagiri.cc>
2018-02-18 00:07:20 +0900

    all functions. images etc still left

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue  = 50
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider : UISlider!
+    @IBOutlet weak var targetLabel : UILabel!
+    @IBOutlet weak var scoreLabel : UILabel!
+    @IBOutlet weak var roundLabel : UILabel!
+    
+    func startNewRound() {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(50)
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,16 +46,50 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World",
-        message: "This is my first app!",
+        let difference = abs(currentValue - targetValue)
+        
+        var points = 100 - difference
+        let title : String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            points += 50
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        score += points
+        
+        let alert = UIAlertController(title: title,
+        message: "The value of the slider is \(currentValue) \n" +
+            "The target value is \(targetValue)" + "\nYou scored \(points) points.",
         preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default,
-                                   handler: nil)
+                                   handler: {action in self.startNewRound()})
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+//        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        print("The value of the slider is now \(sender.value)")
+        currentValue = lroundf(sender.value)
+    }
+    
+    @IBAction func startOver() {
+        currentValue  = 50
+        targetValue = 0
+        score = 0
+        round = 0
+        startNewRound()
     }
 }
 
